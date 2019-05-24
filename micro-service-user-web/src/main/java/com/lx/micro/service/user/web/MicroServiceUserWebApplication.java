ea5f2616b31e2c8d5160ab825bd3cd582191db3e
lanxing <113087685@qq.com>
2019-05-25 00:53:29 +0800

    增加分布式配置

--- micro-service-user-web/src/main/java/com/lx/micro/service/user/web/MicroServiceUserWebApplication.java
+++ micro-service-user-web/src/main/java/com/lx/micro/service/user/web/MicroServiceUserWebApplication.java
@@ -1,5 +1,6 @@
 package com.lx.micro.service.user.web;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author lanxing
  */
 @SpringBootApplication
+@NacosPropertySource(dataId = "example", autoRefreshed = true)
 public class MicroServiceUserWebApplication {
 
     public static void main(String[] args) {
--- micro-service-user-web/src/main/java/com/lx/micro/service/user/web/controller/UserController.java
+++ micro-service-user-web/src/main/java/com/lx/micro/service/user/web/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.lx.micro.service.user.web.controller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.lx.micro.service.user.api.entity.User;
 import com.lx.micro.service.user.api.service.UserService;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * @author lanxing
@@ -14,6 +19,15 @@ public class UserController {
 
     @Reference(check = false,version = "${demo.service.version}")
     private UserService userService;
+
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @RequestMapping(value = "/get", method = GET)
+    @ResponseBody
+    public boolean get() {
+        return useLocalCache;
+    }
 
     @GetMapping("getUser")
     public User getUser(){
