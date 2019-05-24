0400e2f4154c2bacf475daf4573e551fa7577551
LockedX <1130876885@qq.com>
2019-05-24 16:45:12 +0800

    初始化

---
+++ micro-service-user-provider/src/main/java/com/lx/microserviceuserprovider/MicroServiceUserProviderApplication.java
@@ -0,0 +1,16 @@
+package com.lx.microserviceuserprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lanxing
+ */
+@SpringBootApplication
+public class MicroServiceUserProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceUserProviderApplication.class, args);
+    }
+
+}
---
+++ micro-service-user-web/src/main/java/com/lx/microserviceuserweb/MicroServiceUserWebApplication.java
@@ -0,0 +1,16 @@
+package com.lx.microserviceuserweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lanxing
+ */
+@SpringBootApplication
+public class MicroServiceUserWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceUserWebApplication.class, args);
+    }
+
+}
