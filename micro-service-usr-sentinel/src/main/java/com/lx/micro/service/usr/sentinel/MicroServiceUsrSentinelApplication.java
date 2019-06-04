e3a9a40d195222f9ec8656f5deec246ee7566579
LockedX <1130876885@qq.com>
2019-06-04 18:27:27 +0800

    增加限流

---
+++ micro-service-usr-sentinel/src/main/java/com/lx/micro/service/usr/sentinel/MicroServiceUsrSentinelApplication.java
@@ -0,0 +1,16 @@
+package com.lx.micro.service.usr.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lanxing
+ */
+@SpringBootApplication
+public class MicroServiceUsrSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceUsrSentinelApplication.class, args);
+    }
+
+}
