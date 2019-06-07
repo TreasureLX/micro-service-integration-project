98541f6167dc4c341d187882949cdfd1b0694255
lanxing <1130876885@qq.com>
2019-06-07 14:00:33 +0800

    整合Nacos和Sentinel

--- micro-service-usr-sentinel/src/main/java/com/lx/micro/service/usr/sentinel/controller/UserController.java
+++ micro-service-usr-sentinel/src/main/java/com/lx/micro/service/usr/sentinel/controller/UserController.java
@@ -3,6 +3,9 @@ package com.lx.micro.service.usr.sentinel.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author lanxing
+ */
 @RestController
 public class UserController {
 
